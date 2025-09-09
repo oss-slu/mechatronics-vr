788bd9c2d7d81c78d4c23182f001efafe990cdff
Anthony <157141848+AanthonyRusso@users.noreply.github.com>
2025-09-09 16:44:55 -0500

    Added base LessonStep class

---
+++ Source/MechatronicsVR/Lesson/LessonStep.cpp
@@ -0,0 +1,110 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "LessonStep.h"
+
+
+ULessonStep::ULessonStep()
+{
+	bStepCompleted = false;
+	bIsActive = false;
+}
+
+
+
+void ULessonStep::StartStep()
+{
+	if (bIsActive)
+	{
+		return;
+	}
+
+	bIsActive = true;
+	bStepCompleted = false;
+
+	OnStarted();
+	OnStepStarted.Broadcast(this);
+
+	if (checkCompletion())
+	{
+		CompleteStep();
+	}
+}
+
+void ULessonStep::StopStep()
+{
+	if (!bIsActive)
+	{
+		return;
+	}
+
+	OnStopped();
+	bIsActive = false;
+}
+
+void ULessonStep::ResetStep()
+{
+	bIsActive = false;
+	bStepCompleted = false;
+
+	OnReset();
+	OnStepReset.Broadcast(this);
+	NotifyUpdated();
+	
+}
+
+void ULessonStep::TickStep(float DeltaTime)
+{
+	if (!bIsActive || !bWantsTickWhileActive || bStepCompleted)
+	{
+		return;
+	}
+	if (CheckCompletion())
+	{
+		CompleteStep();
+	}
+}
+
+bool ULessonStep::CheckCompletion_Implementation() const
+{
+	return false; // should be defined in subclasses
+}
+
+void ULessonStep::CompleteStep()
+{
+	if (bStepCompleted)
+	{
+		return;
+	}
+	bStepCompleted = true;
+
+	OnStepCompleted.Broadcast(this);
+}
+
+void ULessonStep::NotifyUpdated()
+{
+	OnStepUpdated.Broadcast(this);
+}
+
+void ULessonStep::OnStarted()
+{
+	//to be defined in subclass
+}
+
+void ULessonStep::onStopped()
+{
+	//to be defined in subclass
+}
+
+void ULessonStep::OnReset()
+{
+	// to be defined in subclass
+}
+
+
+
+
+
+
+
+
