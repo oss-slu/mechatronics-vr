2827d1e2301f826c1a1b7bbd7a37f0afc4e5cf88
AanthonyRusso <157141848+AanthonyRusso@users.noreply.github.com>
2026-03-17 19:26:48 -0500

    adding quiz viewer

--- Source/MechatronicsVR/Lesson/QuizLessonStep.cpp
+++ Source/MechatronicsVR/Lesson/QuizLessonStep.cpp
@@ -2,24 +2,147 @@
 
 
 #include "QuizLessonStep.h"
+#include "Engine/Engine.h"
 
-UQuizLessonStep::UQuizLessonStep() {}
+UQuizLessonStep::UQuizLessonStep()
+{
+	bWantsTickWhileActive = false;
+
+	bRequirePassingScore = false;
+	PassingScorePercent = 70.0f;
+	CurrentQuestionIndex = 0;
+	Score = 0;
+	QuizState = EQuizState::WaitingToStart;
+
+	StepType = ELessonStepType::Quiz;
+}
 
 
 
-void UQuizLessonStep::OnStarted() {}
-void UQuizLessonStep::OnStopped() {}
-void UQuizLessonStep::OnReset() {}
+void UQuizLessonStep::OnStarted()
+{
+	if (Questions.Num() == 0)
+	{
+		UE_LOG(LogTemp, Error, TEXT("Quiz step does not have any questions!"));
+		return;
+	}
+	// Resetting just in case the step needs to be restarted
+	CurrentQuestionIndex = 0;
+	Score = 0;
+	QuizState = EQuizState::ShowingQuestion;
+	UE_LOG(LogTemp, Log, TEXT("Quiz has started"));
+}
+void UQuizLessonStep::OnStopped()
+{
+	return;
+}
+void UQuizLessonStep::OnReset()
+{
+	CurrentQuestionIndex = 0;
+	Score = 0;
+	QuizState = EQuizState::WaitingToStart;
+	QuestionResults.Empty();
+}
 
 
-void UQuizLessonStep::SubmitAnswer(int32 ChoiceIndex) {}
+void UQuizLessonStep::SubmitAnswer(int32 ChoiceIndex)
+{
+	if (!bIsActive || QuizState != EQuizState::ShowingQuestion)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Answer is not ready to be submitted"));
+		return;
+	}
+	if (!Questions.IsValidIndex(ChoiceIndex))
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Questions is not valid"));
+	}
+	EvaluateAnswerSubmission(ChoiceIndex);
+}
 
-FQuizQuestion UQuizLessonStep::GetCurrentQuestion() {return FQuizQuestion();}
-FString UQuizLessonStep::GetProgressText() {return FString();}
-FString UQuizLessonStep::GetScoreText() { return FString{};}
-void UQuizLessonStep::EvaluateAnswerSubmission(int32 ChoiceIndex) {}
-void UQuizLessonStep::FinishQuiz() {}
+void UQuizLessonStep::EvaluateAnswerSubmission(int32 ChoiceIndex)
+{
+	const FQuizQuestion& CurrentQuestion  = Questions[CurrentQuestionIndex];
 
-bool UQuizLessonStep::CheckCompletion_Implementation() const { return false; }
+	if (!CurrentQuestion.Choices.IsValidIndex(ChoiceIndex))
+	{
+		UE_LOG (LogTemp, Warning, TEXT("Questions is not valid"));
+		return;
+	}
+
+	FString CorrectChoiceText;
+
+	for (const FQuizChoice& Choice : CurrentQuestion.Choices)
+	{
+		if (Choice.bIsCorrect)
+		{
+			CorrectChoiceText = Choice.ChoiceText;
+			break;
+		}
+	}
+
+	FQuizQuestionResult Result;
+	Result.QuestionText = CorrectChoiceText;
+	Result.SelectedChoiceText =	CurrentQuestion.Choices[ChoiceIndex].ChoiceText;
+	Result.CorrectChoiceText = CorrectChoiceText;
+	Result.bAnsweredCorrectly = CurrentQuestion.Choices[ChoiceIndex].bIsCorrect;
+
+	if (Result.bAnsweredCorrectly)
+	{
+		++Score;
+	}
+
+	const bool BLastQuestion = CurrentQuestionIndex >= Questions.Num() - 1;
+	if (BLastQuestion)
+	{
+		FinishQuiz();
+	}
+	else
+	{
+		++CurrentQuestionIndex;
+		NotifyUpdated();
+	}
+}
+void UQuizLessonStep::FinishQuiz()
+{
+	QuizState = EQuizState::ShowingResults;
+
+	NotifyUpdated();
+	if (CheckCompletion_Implementation())
+	{
+		CompleteStep();
+	}
+}
+
+FQuizQuestion UQuizLessonStep::GetCurrentQuestion()
+{
+	if (Questions.IsValidIndex(CurrentQuestionIndex))
+	{
+		return Questions[CurrentQuestionIndex];
+	}
+	UE_LOG(LogTemp, Warning, TEXT("Questions is not valid"));
+
+	return FQuizQuestion{};
+}
+FString UQuizLessonStep::GetProgressText()
+{
+	return FString::Printf(TEXT("Question %d of %d"), CurrentQuestionIndex + 1, Questions.Num());
+}
+FString UQuizLessonStep::GetScoreText()
+{
+	return FString::Printf(TEXT("%d / %d Correct"), Score, Questions.Num());
+}
+
+
+bool UQuizLessonStep::CheckCompletion_Implementation() const
+{
+	if (QuizState != EQuizState::ShowingResults && QuizState != EQuizState::ShowingQuestion)
+	{
+		return false;
+	}
+
+	// Maybe add check to see if the person did not get a passing score?
+	
+	return true;
+}
 
 
