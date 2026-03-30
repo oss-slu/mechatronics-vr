3bc1ab9c0b0816d535558d62ec8003d7b501356c
AanthonyRusso <157141848+AanthonyRusso@users.noreply.github.com>
2026-03-29 21:55:26 -0500

    quiz now ends and shows score

--- Source/MechatronicsVR/Lesson/LessonManagerComponent.cpp
+++ Source/MechatronicsVR/Lesson/LessonManagerComponent.cpp
@@ -137,6 +137,7 @@ bool ULessonManagerComponent::InitializeLessonFromDataAsset(ULessonDataAsset* Le
 		if (StepData.StepInstance)
 		{
 			LessonSteps.Add(StepData.StepInstance);
+			StepData.StepInstance->ResetStep();
         
 			// Clear any existing bindings first
 			StepData.StepInstance->OnStepCompleted.Clear();
