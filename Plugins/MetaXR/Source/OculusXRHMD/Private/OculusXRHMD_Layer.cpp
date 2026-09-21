98209b261919930162bc110ed3a03d6a15455d21
GitCole <coleyo34@gmail.com>
2026-09-21 17:30:19 -0500

    Fix Meta XR settings and build compilation error

--- Plugins/MetaXR/Source/OculusXRHMD/Private/OculusXRHMD_Layer.cpp
+++ Plugins/MetaXR/Source/OculusXRHMD/Private/OculusXRHMD_Layer.cpp
@@ -1371,7 +1371,7 @@ namespace OculusXRHMD
 			{
 				OvrpLayerSubmit.LayerSubmitFlags |= ovrpLayerSubmitFlag_SpaceWarp;
 				OvrpLayerSubmit.EyeFov.MotionVectorDepthFar = Frame->NearClippingPlane / 100.f;
-				OvrpLayerSubmit.EyeFov.MotionVectorDepthNear = INFINITY;
+				OvrpLayerSubmit.EyeFov.MotionVectorDepthNear = 1;
 				OvrpLayerSubmit.EyeFov.MotionVectorOffset = ovrpVector4f{ 0.0f, 0.0f, 0.0f, 0.0f };
 				OvrpLayerSubmit.EyeFov.MotionVectorScale = ovrpVector4f{ 1.0f, 1.0f, 1.0f, 1.0f };
 
